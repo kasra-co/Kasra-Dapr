5f8f6a6a9e56e1a3722d801eb72e7492bbeed640
Jihdeh <jide.b.tade@gmail.com>
2016-09-27 22:57:51 +0100

    take wrong email out from error mail and change my mail to cc

--- config/toEmails.js
+++ config/toEmails.js
@@ -1,6 +1,6 @@
 module.exports = {
   to: ["ali@kasra.co"],
-  cc: ["nawaf@kasra.co"],
+  cc: ["nawaf@kasra.co", "jide@kasra.co"],
   error: {
     to: ["jide@kasra.co"],
     cc: ["nawaf@kasra.co", "ali@kasra.co", "neveen@kasra.co"]
