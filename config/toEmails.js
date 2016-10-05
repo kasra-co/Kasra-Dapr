13b1d09e4c958e7ff8fdbdbaa020e0814dbafaf3
Jihdeh <jide.b.tade@gmail.com>
2016-10-05 22:20:26 +0100

    add alice email

--- config/toEmails.js
+++ config/toEmails.js
@@ -1,5 +1,5 @@
 module.exports = {
-  to: ["jide@kasra.co", "ali@kasra.co"],
+  to: ["jide@kasra.co", "ali@kasra.co", "annacarolin.haleblian@kasra.co"],
   cc: ["nawaf@kasra.co"],
   error: {
     to: ["dev@kasra.co"],
